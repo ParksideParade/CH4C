181564325a56f6fd2f5a4a064df5ff8d01a00a78
JoshF <joshfeira@gmail.com>
2024-08-26 11:40:14 -0700

    pipe stream

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-console.log("Hi world");
+// console.log("Hi world");
 
 const express = require('express')
+const { Readable } = require( "stream" )
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async (req, res) => {
+  // res.send('Hello World!')
+
+  // get the encoder stream
+  const myUrl = 'http://192.168.107.9/live/stream0'
+  const fetchResponse = await fetch(myUrl)
+
+  // send the pipe of the stream to the request
+  Readable.fromWeb(fetchResponse.body).pipe(res)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
