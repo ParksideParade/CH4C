d2cd1be6483b133c17e51e07ebf844cfed7ea6d9
JoshF <82978439+ParksideParade@users.noreply.github.com>
2024-08-26 18:44:46 +0000

    trying to sync

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  //res.send('Hello World!')
+
+  // https://nodejs.org/api/http.html#httpgeturl-options-callback
+  const url = 'http://192.168.107.9/live/stream0';
+  request.get(url).pipe(res);
 })
 
 app.listen(port, () => {
