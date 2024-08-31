f3ee74fcccdaeac18cfe61695775c9cc7b5e8a93
JoshF <joshfeira@gmail.com>
2024-08-31 15:14:12 -0700

    building instant

--- main.js
+++ main.js
@@ -46,13 +46,41 @@ const getCurrentBrowser = async () => {
 
   return currentBrowser
 }
-
+/*
+async function launchBrowser(videoUrl) {
+  try {
+      browser = await getCurrentBrowser()
+      page = await browser.newPage()
+      await page.goto(videoUrl)
+      await page.waitForSelector('video')
+      await page.waitForFunction(`(function() {
+        let video = document.querySelector('video')
+        return video.readyState === 4
+      })()`)
+      await page.evaluate(`(function() {
+          document.body.style.cursor = 'none'
+          let video = document.querySelector('video')
+          video.style.cursor = 'none'
+          video.play()
+          video.muted = false
+          video.removeAttribute('muted')
+          video.requestFullscreen()
+      })()`)
+    } catch (e) {
+      console.log('failed to start browser page', e)
+      res.status(500).send(`failed to start browser page: ${e}`)
+      return
+    }
+}
+*/
 async function main() {
   const app = express()
   const port = 3000
 
   dataDir = '/home/ellefeira/.config/chromium'
 
+  // send back the transcoder stream - testing only
+  // delete when ready
   app.get('/', async (_req, res) => {
     const encoderUrl = 'http://192.168.107.9/live/stream0'
     const fetchResponse = await fetch(encoderUrl)
@@ -61,12 +89,19 @@ async function main() {
 
   // : means query paramter
   // ? means optional
+  // call this path from the custom channel in Channels
+  // passing in either a name or a URL
+  // the custom URL form can also call this, just include
+  // a flag that indicates don't pipe back and send JSON
   app.get('/go', async (_req, res) => {
 
+    // feed the transcoder output back to Channels
     const encoderUrl = 'http://192.168.107.9/live/stream0'
     const fetchResponse = await fetch(encoderUrl)
     Readable.fromWeb(fetchResponse.body).pipe(res)
 
+    // get the desired URL - make that a function
+    //var u = req.query.url
     const videoUrl = 'https://www.nfl.com/plus/games/colts-at-bengals-2024-pre-3?mcpid=1888004'
     var browser, page
 
@@ -75,6 +110,9 @@ async function main() {
       await page.close()
       console.log('finished')
     })
+
+    // try the below as a function
+    // await launchBrowser(videoUrl);
 
     try {
       browser = await getCurrentBrowser()
@@ -99,7 +137,31 @@ async function main() {
       res.status(500).send(`failed to start browser page: ${e}`)
       return
     }
+
   })
+
+  /*
+  app.get('/instant', async (_req, res) => {
+
+    // send JSON so Channels starts recording the transcoder
+    await sendJSON
+
+    // get the desired URL
+    //var u = req.query.url
+    const videoUrl = 'https://www.nfl.com/plus/games/colts-at-bengals-2024-pre-3?mcpid=1888004'
+    var browser, page
+
+    // load the desired URL
+    await launchBrowser(videoUrl);
+
+    // close the browser when the recording is done
+    // https://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
+    await new Promise(r => setTimeout(r, 2000));
+    await page.close()
+    console.log('finished instant recording')
+    
+  })
+  */
 
   const server = app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
